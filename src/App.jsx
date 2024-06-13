e670a2c28e93422a137e29f291459a16c6f5de0d
Khandaker Sojol Ahmed <dev.khandakersojol@gmail.com>
2024-06-13 11:26:27 -0700

    blog added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Blogs from './components/Header/Blogs/Blogs'
 import Header from './components/Header/Header'
 
 function App() {
 
   return (
-    <>
+    <div className='max-w-screen-xl mx-auto'>
      <Header></Header>
-     <Blogs></Blogs>
-    </>
+     <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      
+     </div>
+    </div>
   )
 }
 
---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types'; 
+import { CiBookmark } from "react-icons/ci";
+
+const Blog = ({blog}) => {
+    const {title, cover, author, author_img, reading_time, posted_date, hashtags  } = blog;
+    return (
+        <div className='mb-12'>
+            <img className='w-full ' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between m-4'>
+                <div className='flex'>
+                    <img className='w-16 h-14 rounded-full' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div  className='text-2xl flex items-center' >
+                    <span >{reading_time} min read</span>
+                    <button><CiBookmark></CiBookmark></button>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p className='space-x-4 py-4 '>
+                {
+                    hashtags.map ((hash, idx) => <span className='text-blue-800' key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
+            <button className='text-sky-500'><a className='border-b-1' href=""> <u>Mark as Read</u></a></button>
+        </div>
+    );
+};
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired 
+}
+export default Blog;
+
+
+
